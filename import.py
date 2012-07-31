6bc65febc5aff0ceace6a505b59ff434702e48c1
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2012-07-30 22:00:49 -0700

    added social graph features

--- import.py
+++ import.py
@@ -19,12 +19,14 @@ xml_namespaces = {
                   "oa": "http://www.openarchives.org/OAI/2.0/"
                  }
 
-
+#Query the index to find the resource, create node if not exist
 def save_resource_node(envelope, db, idx):
+    #print 'ENVELOPE - ' + str(envelope) + '\n'
     try:
         found_items = idx.query('resource:' + urllib.quote_plus(envelope['resource_locator']))
     except Exception:
         found_items = []
+
     if  len(found_items) > 0:
         new_node = found_items[0]
     else:
@@ -32,17 +34,27 @@ def save_resource_node(envelope, db, idx):
         idx['resource'][urllib.quote_plus(envelope['resource_locator'])] = new_node
     return new_node
 
-
+#Retrieve conforms to data from LR data
 def get_conforms_to_data(envelope):
     xml = etree.fromstring(envelope['resource_data'])
     return (StandardsRelationship(standard=urllib.quote_plus(x.text), relation='conformsTo') for x in xml.xpath("./dct:conformsTo", namespaces=xml_namespaces))
 
+def get_conforms_to_submitter_data(envelope):
+    xml = etree.fromstring(envelope['resource_data'])
+    for x in xml.xpath("./dc:creator", namespaces=xml_namespaces):
+        return x.text  
 
+#Get related paradata that has a standard
 def get_paradata_standards_data(envelope):
     para = envelope['resource_data']['activity']
     for related in para['related']:
         if related['objectType'].lower() == 'academic standard':
             yield StandardsRelationship(standard=urllib.quote_plus(related['id']), relation=urllib.quote_plus(para['verb']['action']))
+
+def get_paradata_actor_data(envelope):
+    para = envelope['resource_data']['activity']
+    if 'displayName' in para['actor'].keys():
+        return para['actor']['displayName']
 
 
 def process_conforms_to_data(conforms_to, db, idx, new_node):
@@ -60,10 +72,13 @@ def process_conforms_to_data(conforms_to, db, idx, new_node):
             idx['standard'][i.standard] = cc_node
         new_node.relationships.create(i.relation, cc_node)
 
-
-def save_data(data_set, db, idx, get_conforms_to_data_from_envelope):
+#Create new node with data
+def save_data(data_set, db, idx, get_conforms_to_data_from_envelope, submitter_func=None):
     for envelope in data_set:
         new_node = save_resource_node(envelope, db, idx)
+        if submitter_func is not None:
+            submitter_node = db.nodes.create(submitter=submitter_func(envelope))
+            submitter_node.submitted(new_node)
         process_conforms_to_data(get_conforms_to_data_from_envelope(envelope), db, idx, new_node)
 
 
@@ -95,6 +110,7 @@ def process_cc_standards(db, idx):
             return standard_query[0]
         else:
             return db.nodes.create(standard=urllib.quote_plus(standard))
+    
     with open('E0330_ccss_identifiers.csv', 'rU') as f:
         dr = DictReader(f)
         for row in dr:
@@ -121,37 +137,56 @@ def process_purl_data(db, idx, urls, ids):
 
 
 def init_neo4j(url):
+    #Initialize DB
     db = GraphDatabase(url)
+    
+    #If resources index already exists, grab it, else create it
     if INDEX_NAME in db.nodes.indexes:
         idx = db.nodes.indexes.get(INDEX_NAME)
     else:
         idx = db.nodes.indexes.create(INDEX_NAME, type="fulltext")
     return db, idx
 
-
-def process_data_service(results, db, idx, conforms_func):
+#Get the resource_data from each result and save 
+def process_data_service(results, db, idx, conforms_func, submitter_func=None):
     for result_item in results:
-        save_data(result_item['resource_data'], db, idx, conforms_func)
+        #print 'RESULT ITEM - ' + str(result_item)
+        save_data(result_item['resource_data'], db, idx, conforms_func, submitter_func)
 
 
 def main(args):
     urls = [('Literacy', 'http://asn.jesandco.org/resources/D10003FC_manifest.json'),
             ("Math", "http://asn.jesandco.org/resources/D10003FB_manifest.json")]
     whitelist = ['matched', 'recommended', 'aligned']
+    
+    #Initialize DB and index
     db, idx = init_neo4j(args.db)
-    # results = requests.get(args.url)
-    # results = items(results.raw, 'documents.item')
-    # process_data_service(results, db, idx, get_conforms_to_data)
+    
+    #Get conformsT0 LR data
+    results = requests.get(args.url)
+    results = items(results.raw, 'documents.item')
 
-    # def filter_paradata(item):
-    #     valid = True
-    #     for x in item['resource_data']:
-    #         valid = valid or x['resource_data']['verb']['action'] in whitelist
-    #     return valid
-    # results = requests.get(args.para)
-    # results = (x for x in items(results.raw, 'documents.item') if filter_paradata(x))
-    # process_data_service(results, db, idx, get_paradata_standards_data)
+    #Save conformsTo data
+    process_data_service(results, db, idx, get_conforms_to_data, get_conforms_to_submitter_data)
+
+    #Return paradata matching to matched, recommended, or aligned
+    def filter_paradata(item):
+        valid = True
+        for x in item['resource_data']:
+            valid = valid or x['resource_data']['verb']['action'] in whitelist
+        return valid
+    
+    #Get paradata
+    results = requests.get(args.para)
+    results = (x for x in items(results.raw, 'documents.item') if filter_paradata(x))
+
+    #Send in valid paradata standards data
+    process_data_service(results, db, idx, get_paradata_standards_data, get_paradata_actor_data)
+    
+        
     ids = process_cc_standards(db, idx)
+
+
     process_purl_data(db, idx, urls, ids)
 
 if __name__ == "__main__":
