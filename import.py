c4082707c8c33dd3c8b7f70c3840d1b9ee602b55
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2012-08-21 12:45:00 -0700

    added social graph, and when creating submitter nodes add fields that are url encoded

--- import.py
+++ import.py
@@ -7,9 +7,12 @@ from csv import DictReader
 from lxml import etree
 from ijson import items
 import urllib
+import types
+
 from collections import namedtuple
 StandardsRelationship = namedtuple("StandardsRelationship", ['standard', 'relation'])
-INDEX_NAME = 'resources'
+RESOURCE_INDEX_NAME = 'resources'
+SUBMITTER_INDEX_NAME = 'submitters'
 xml_namespaces = {
                   "dc": "http://purl.org/dc/elements/1.1/",
                   "dct": "http://purl.org/dc/terms/",
@@ -21,7 +24,6 @@ xml_namespaces = {
 
 #Query the index to find the resource, create node if not exist
 def save_resource_node(envelope, db, idx):
-    #print 'ENVELOPE - ' + str(envelope) + '\n'
     try:
         found_items = idx.query('resource:' + urllib.quote_plus(envelope['resource_locator']))
     except Exception:
@@ -30,7 +32,7 @@ def save_resource_node(envelope, db, idx):
     if  len(found_items) > 0:
         new_node = found_items[0]
     else:
-        new_node = db.nodes.create(resource=envelope['resource_locator'])
+        new_node = db.nodes.create(resource=urllib.quote_plus(envelope['resource_locator']))
         idx['resource'][urllib.quote_plus(envelope['resource_locator'])] = new_node
     return new_node
 
@@ -41,9 +43,23 @@ def get_conforms_to_data(envelope):
 
 #Retrieve submitter from conforms to data
 def get_conforms_to_submitter_data(envelope):
-    xml = etree.fromstring(envelope['resource_data'])
+    xml = etree.fromstring(envelope['resource_data'])  
+    #Check for creator first
     for x in xml.xpath("./dc:creator", namespaces=xml_namespaces):
-        return x.text  
+        #Only return if it's not none
+        if not isinstance(x.text, types.NoneType):
+            #Might be in unicode, encode to utf-8 and use urllib to escape special characters
+            return urllib.quote_plus(x.text.encode('utf-8'))
+    
+    #Check for publisher if no creator
+    for y in xml.xpath("./dc:publisher", namespaces=xml_namespaces):
+        #Only return if it's not none
+        if not isinstance(y.text, types.NoneType):
+            #Might be in unicode, encode to utf-8 and use urllib to escape special characters            
+            return urllib.quote_plus(y.text.encode('utf-8'))
+
+    #Return none if neither are found
+    return None
 
 #Get related paradata that has a standard
 def get_paradata_standards_data(envelope):
@@ -56,8 +72,9 @@ def get_paradata_standards_data(envelope):
 def get_paradata_actor_data(envelope):
     para = envelope['resource_data']['activity']
     if 'displayName' in para['actor'].keys():
-        return para['actor']['displayName']
-
+        return urllib.quote_plus(para['actor']['displayName'])
+    else:
+        return None
 
 def process_conforms_to_data(conforms_to, db, idx, new_node):
     for i in conforms_to:
@@ -75,13 +92,17 @@ def process_conforms_to_data(conforms_to, db, idx, new_node):
         new_node.relationships.create(i.relation, cc_node)
 
 #Create new node with data
-def save_data(data_set, db, idx, get_conforms_to_data_from_envelope, submitter_func=None):
+def save_data(data_set, db, ridx, sidx, get_conforms_to_data_from_envelope, submitter_func=None):
     for envelope in data_set:
-        new_node = save_resource_node(envelope, db, idx)
+        new_node = save_resource_node(envelope, db, ridx)
         if submitter_func is not None:
-            submitter_node = db.nodes.create(submitter=submitter_func(envelope))
-            submitter_node.submitted(new_node)
-        process_conforms_to_data(get_conforms_to_data_from_envelope(envelope), db, idx, new_node)
+            #Only create submitter_node if there is data supplied in envelope
+            if submitter_func(envelope):
+                submitter_node = db.nodes.create(submitter=submitter_func(envelope))
+                submitter_node.submitted(new_node)
+                sidx['submitter'][submitter_func(envelope)] = submitter_node
+        
+        process_conforms_to_data(get_conforms_to_data_from_envelope(envelope), db, ridx, new_node)
 
 
 def parse_standards_data(data, prefix, valid_ids):
@@ -143,16 +164,24 @@ def init_neo4j(url):
     db = GraphDatabase(url)
     
     #If resources index already exists, grab it, else create it
-    if INDEX_NAME in db.nodes.indexes:
-        idx = db.nodes.indexes.get(INDEX_NAME)
+    if RESOURCE_INDEX_NAME in db.nodes.indexes:
+        ridx = db.nodes.indexes.get(RESOURCE_INDEX_NAME)
     else:
-        idx = db.nodes.indexes.create(INDEX_NAME, type="fulltext")
-    return db, idx
+        ridx = db.nodes.indexes.create(RESOURCE_INDEX_NAME, type="fulltext")
+
+    #If resources index already exists, grab it, else create it
+    if SUBMITTER_INDEX_NAME in db.nodes.indexes:
+        sidx = db.nodes.indexes.get(SUBMITTER_INDEX_NAME)
+    else:
+        sidx = db.nodes.indexes.create(SUBMITTER_INDEX_NAME, type="fulltext")
+
+
+    return db, ridx, sidx
 
 #Get the resource_data from each result and save 
-def process_data_service(results, db, idx, conforms_func, submitter_func=None):
+def process_data_service(results, db, ridx, sidx, conforms_func, submitter_func=None):
     for result_item in results:
-        save_data(result_item['resource_data'], db, idx, conforms_func, submitter_func)
+        save_data(result_item['resource_data'], db, ridx, sidx, conforms_func, submitter_func)
 
 
 def main(args):
@@ -163,14 +192,14 @@ def main(args):
     whitelist = ['matched', 'recommended', 'aligned']
     
     #Initialize DB and index
-    db, idx = init_neo4j(args.db)
+    db, ridx, sidx = init_neo4j(args.db)
     
     #Get conformsTo LR data
     results = requests.get(args.url)
     results = items(results.raw, 'documents.item')
 
     #Save conformsTo data
-    process_data_service(results, db, idx, get_conforms_to_data, get_conforms_to_submitter_data)
+    process_data_service(results, db, ridx, sidx, get_conforms_to_data, get_conforms_to_submitter_data)
 
     #Return paradata matching to matched, recommended, or aligned
     def filter_paradata(item):
@@ -184,13 +213,13 @@ def main(args):
     results = (x for x in items(results.raw, 'documents.item') if filter_paradata(x))
 
     #Send in valid paradata standards data
-    process_data_service(results, db, idx, get_paradata_standards_data, get_paradata_actor_data)
+    process_data_service(results, db, ridx, sidx, get_paradata_standards_data, get_paradata_actor_data)
     
     #Create nodes from cc standards    
-    ids = process_cc_standards(db, idx)
+    ids = process_cc_standards(db, ridx)
 
     #Create relationships from purl data
-    process_purl_data(db, idx, urls, ids)
+    process_purl_data(db, ridx, urls, ids)
 
 #Add args in main for conformsTo and paradata URLs to harvest from
 if __name__ == "__main__":
